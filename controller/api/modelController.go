f211cce4fbc2038184c784e0d49267ff01a4487d
Sungindra <sungindra.setiawan@gmail.com>
2024-02-01 18:54:14 +0700

    add api path

---
+++ controller/api/modelController.go
@@ -0,0 +1,7 @@
+package api
+
+import "net/http"
+
+func GetModels(w http.ResponseWriter, r *http.Request) {
+
+}
--- handler/router.go
+++ handler/router.go
@@ -10,10 +10,20 @@ import (
 
 func RouteHandler() {
 	r := chi.NewRouter()
+
 	r.Get("/", controller.Index)
-	r.Route("/model", func(r chi.Router) {
-		r.Get("/", controller.ListModel)
-		r.Get("/{id}", controller.GetModel)
+
+	r.Route("/cms", func(r chi.Router) {
+		r.Route("/model", func(r chi.Router) {
+			r.Get("/", controller.ListModel)
+			r.Get("/{id}", controller.GetModel)
+		})
+	})
+
+	r.Route("/api", func(r chi.Router) {
+		r.Route("/model", func(r chi.Router) {
+			r.Get("/", nil)
+		})
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", r))
