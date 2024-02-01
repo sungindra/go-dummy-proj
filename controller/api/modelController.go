e225cfb658bb1b93ea147e10a7d9396818808421
Sungindra <sungindra.setiawan@gmail.com>
2024-02-01 19:08:00 +0700

    API controller

--- controller/api/modelController.go
+++ controller/api/modelController.go
@@ -1,7 +1,11 @@
 package api
 
-import "net/http"
+import (
+	"dummy/mock"
+	"encoding/json"
+	"net/http"
+)
 
 func GetModels(w http.ResponseWriter, r *http.Request) {
-
+	json.NewEncoder(w).Encode(mock.MockModels())
 }
