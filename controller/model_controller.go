5e568f6cabc15ac47f720408cdb897de5a5b35dd
Giovanni Dejan <15686093+iamdejan@users.noreply.github.com>
2024-02-02 11:39:09 +0700

    Change list view tmpl

--- controller/model_controller.go
+++ controller/model_controller.go
@@ -8,11 +8,11 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
-var tmpl = template.Must(template.ParseFiles("view/model/listview.html"))
+var listViewTmpl = template.Must(template.ParseFiles("view/model/listview.html"))
 
 func ListModel(w http.ResponseWriter, r *http.Request) {
 	models := repository.GetModels()
-	tmpl.Execute(w, models)
+	listViewTmpl.Execute(w, models)
 }
 
 func GetModel(w http.ResponseWriter, r *http.Request) {
